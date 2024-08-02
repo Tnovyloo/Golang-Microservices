40620ae1ca24932741c7f84d55f6c2682d00078a
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-08-02 23:32:01 +0200

    Creating small microservice

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
+}
+
+func (app *Config) GetBroker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   true,
+		Message: "Hitted get method ;)",
+	}
+
+	log.Printf(r.Method)
+
+	out, _ := json.MarshalIndent(payload, "", "\t")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(out)
+
+}
